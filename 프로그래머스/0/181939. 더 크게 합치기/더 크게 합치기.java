be29d3f9d900f94a0d3bfbc69cb682bfd8bd2951
백다연 <112463603+bdy4119@users.noreply.github.com>
2025-06-10 16:00:25 +0900

    [level 0] Title: 더 크게 합치기, Time: 17.64 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        int num1 = Integer.parseInt(a + "" + b);
+        int num2 = Integer.parseInt(b + "" + a);
+        
+        if(num1 > num2 || num1 == num2) answer = num1;
+        else if(num1 < num2) answer = num2;
+        
+        return answer;
+    }
+}
