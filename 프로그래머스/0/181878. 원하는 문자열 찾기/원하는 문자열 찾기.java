de1e68340bc888424262233156110ac57f3c5d2e
백다연 <112463603+bdy4119@users.noreply.github.com>
2025-06-10 16:38:38 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 12.62 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        
+        String upperMyString = myString.toUpperCase();
+        String upperPat = pat.toUpperCase();
+        
+        if(upperMyString.contains(upperPat)) answer = 1;
+        
+        return answer;
+    }
+}
