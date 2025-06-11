7a2be8a63e7ae499e3f3742fcbf4892ea5ab2a8c
백다연 <112463603+bdy4119@users.noreply.github.com>
2025-06-11 17:40:11 +0900

    [level 0] Title: 수 조작하기 2, Time: 1144.23 ms, Memory: 385 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String solution(int[] numLog) {
+        
+        // 조작 전, 처음에 주어지는 숫자
+        int num = numLog[0];
+        String answer = "";
+        
+        for(int i = 1; i < numLog.length; i++) {
+            if(num + 1 == numLog[i]) {
+                num = numLog[i];
+                answer += "w";
+            }
+            else if(num - 1 == numLog[i]) {
+                num = numLog[i];
+                answer += "s";
+            }
+            else if(num + 10 == numLog[i]) {
+                num = numLog[i];
+                answer += "d";
+            }
+            else if(num - 10 == numLog[i]) {
+                num = numLog[i];
+                answer += "a";
+            }
+        }
+        
+        return answer;
+    }
+}
