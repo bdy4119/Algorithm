7d7e48dc91582e14a30c226825187347ab6b9b6f
백다연 <112463603+bdy4119@users.noreply.github.com>
2025-06-12 11:25:24 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.39 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,16 +1,17 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
+        int answer = 1;
         
-        int sum = a + b + c;    // 합
-        int squareSum = (int) (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));   // 제곱의 합
-        int cubeSum = (int) (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));     // 세제곱의 합
+        // Set은 중복제거해줌
+        // result의 size 에 따라 결과값 구별
+        // size == 3 > 다 다름 / size == 2 > 두개는 같음 / size == 1 > 모두 같은 수
+        Set<Integer> result = Stream.of(a, b, c).collect(Collectors.toSet());
         
-        if(a != b && a != c && b != c) answer = sum;
-        else if( (a == b && a != c) 
-                    || (b == c && b != a)
-                    || (a == c && a != b)) answer = sum * squareSum;
-        else if(a == b && a == c) answer = sum * squareSum * cubeSum;
+        for(int i = 1; i <= 4 - result.size(); i++) 
+            answer *= (int) (Math.pow(a, i) + Math.pow(b, i) + Math.pow(c, i));
         
         return answer;
     }
