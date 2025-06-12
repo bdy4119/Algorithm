078c94e80579c8b1e84ca4c9689f6b2fa10656ab
백다연 <112463603+bdy4119@users.noreply.github.com>
2025-06-12 13:45:33 +0900

    [level 0] Title: l로 만들기, Time: 21.76 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,13 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        
-        int charINum = (int) 'l';
-        for(int i = 0; i < myString.length(); i++) {
-            if( (int) myString.charAt(i) < charINum ) answer += "l";
-            else answer += myString.charAt(i) + "";
-        }
-        
-        return answer;
+        return myString.replaceAll("[a-k]", "l");
     }
 }
