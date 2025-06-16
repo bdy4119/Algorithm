f07802251edcff535410403a0e496d15be767624
백다연 <112463603+bdy4119@users.noreply.github.com>
2025-06-16 15:25:55 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 7.42 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -4,7 +4,7 @@ class Solution {
             int s = queries[j][0];
             int e = queries[j][1];
                 
-            for(int i = 0; i < arr.length; i++) if(s <= i && i <= e) arr[i] += 1;
+            for(int i = 0; i < arr.length; i++) if(s <= i && i <= e) arr[i]++;
         }
         
         return arr;
