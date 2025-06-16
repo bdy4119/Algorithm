5c42a22d71276249d5e76295956f82e8379755d4
백다연 <112463603+bdy4119@users.noreply.github.com>
2025-06-16 15:03:53 +0900

    [level 0] Title: 코드 처리하기, Time: 17.23 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,19 +1,13 @@
 class Solution {
     public String solution(String code) {
-        String mode = "0";
+        boolean mode = false;
         StringBuilder ret = new StringBuilder();
         
         for(int i = 0; i < code.length(); i++) {
             char str = code.charAt(i);
-        
-            if(mode.equals("0")) {
-                if(str == '1') mode = "1";
-                else ret.append(i % 2 == 0 ? str : "");
-            }
-            else {
-                if(str == '1') mode = "0";
-                else ret.append(i % 2 != 0 ? str : "");
-            }
+            
+            if(str == '1') mode = !mode;
+            else ret.append((i % 2 != 0 && mode) || (i % 2 == 0 && !mode) ? str : "");
         }
         
         return ret.toString().isEmpty() ? "EMPTY" : ret.toString();
