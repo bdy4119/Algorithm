dbe6ab5e28923caab335e99fe8b2732906356e7c
백다연 <112463603+bdy4119@users.noreply.github.com>
2025-06-17 18:25:23 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -5,6 +5,8 @@ class Solution {
         
         // 홀수
         for(int i = 0; i < num_list.length; i +=2) odd += num_list[i];
+
+        // 짝수
         for(int i = 1; i < num_list.length; i +=2) even += num_list[i];
         
         return odd > even ? odd : even;
