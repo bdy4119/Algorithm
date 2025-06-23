231302ee91af21b61f4ee7ab68cfbb32f18ee245
백다연 <112463603+bdy4119@users.noreply.github.com>
2025-06-23 16:54:48 +0900

    [level 0] Title: 문자열 묶기, Time: 6.70 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -3,13 +3,9 @@ class Solution {
         int answer = 0;
         
         int[] maxArr = new int[30];
-        for(int i = 0; i < strArr.length; i++) {
-            maxArr[strArr[i].length() - 1] += 1; 
-        }
+        for(int i = 0; i < strArr.length; i++) maxArr[strArr[i].length() - 1] += 1; 
         
-        for(int max : maxArr) {
-            if(max > 0 && answer < max) answer = max;
-        }
+        for(int max : maxArr) if(max > 0 && answer < max) answer = max;
         
         return answer;
     }
