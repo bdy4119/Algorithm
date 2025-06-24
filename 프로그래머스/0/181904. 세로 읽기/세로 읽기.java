5dc0eaba54691984f160353008f70c4b349b2e17
백다연 <112463603+bdy4119@users.noreply.github.com>
2025-06-24 18:14:46 +0900

    [level 0] Title: 세로 읽기, Time: 3.44 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -4,13 +4,9 @@ class Solution {
         
         String[] strArr = new String[my_string.length() / m + my_string.length() % m];
         
-        int idx = 0;
         for(int i = 0; i < my_string.length(); i += m) {
-            strArr[idx++] = my_string.substring(i, i + m);
-        }
-        
-        for(String str : strArr) {
-            answer += str.substring(c-1, c);
+            String str = my_string.substring(i, i + m);
+            answer += str.substring(c - 1, c);
         }
         
         return answer;
